07d4ed6a66d78b124ed5dbced667e3e53aec348d
Arthur-codeX <daniel.karanja239@gmail.com>
2022-02-14 04:18:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+
+const countDown = (n) => {
+  while (n > -1) {
+    console.log(n);
+    n--;
+  }
+};
+
+const writeCards = (docs, name) =>
+  docs.map((doc) => {
+    return `Thank you, ${doc}, for the wonderful ${name} gift!`;
+  });
